7a122433944af0b117675b36563ed0c688397c2a
SANGCO <nsg1213@gmail.com>
2024-03-18 17:04:55 +0900

    feat : [7장] Processor API 예제 추가

---
+++ ch07/digital-twin/src/main/java/com/magicalpipelines/VitalTimestampExtractor.java
@@ -0,0 +1,25 @@
+package com.magicalpipelines;
+
+import com.magicalpipelines.model.Vital;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.streams.processor.TimestampExtractor;
+
+import java.time.Instant;
+
+/**
+ * This class allows us to use event-time semantics for purchase streams
+ */
+public class VitalTimestampExtractor implements TimestampExtractor {
+
+    @Override
+    public long extract(ConsumerRecord<Object, Object> record, long partitionTime) {
+        Vital measurement = (Vital) record.value();
+        if (measurement != null && measurement.getTimestamp() != null) {
+            String timestamp = measurement.getTimestamp();
+            // System.out.println("Extracting timestamp: " + timestamp);
+            return Instant.parse(timestamp).toEpochMilli();
+        }
+        // fallback to stream time
+        return partitionTime;
+    }
+}
